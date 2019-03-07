02363da9ce0caaa72d1418ee001e5fee97100b4d
WangGuofeng <wangguofeng@live.cn>
2019-03-07 10:27:19 +0800

    简单的单选框Demo
    快速创建分组单选框

--- Demo/YBROptionViewDemo/YBROptionViewDemo/ViewController.m
+++ Demo/YBROptionViewDemo/YBROptionViewDemo/ViewController.m
@@ -7,11 +7,14 @@
 //
 
 #import "ViewController.h"
-//#import <YBROptionView/YBROptionView.h>
-//#import <YBROptionView/y\
->
-#import <ybr>
+
+#import <YBROptionView.h>
+#import <PureLayout.h>
+
 @interface ViewController ()
+{
+    YBROptionGroup *optionGrop;
+}
 
 @end
 
@@ -20,6 +23,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    optionGrop = [[YBROptionGroup alloc] initWithGroupId:@"Group1"];
+    
+    
+    YBROptionView *pOption1View = [[YBROptionView alloc] initWithText:@"Option1"];
+    
+    YBROptionView *pOption2View = [[YBROptionView alloc] initWithText:@"Option2"];
+    
+    [self.view addSubview:pOption1View];
+    [self.view addSubview:pOption2View];
+    
+    [optionGrop addOptionView:pOption1View];
+    [optionGrop addOptionView:pOption2View];
+    
+    
+    [pOption1View autoPinEdgeToSuperviewEdge:ALEdgeLeft withInset:150];
+    [pOption1View autoSetDimensionsToSize:CGSizeMake(100, 30)];
+    [pOption1View autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:50];
+    
+    [pOption2View autoPinEdgeToSuperviewEdge:ALEdgeRight withInset:150];
+    [pOption2View autoSetDimensionsToSize:CGSizeMake(100, 30)];
+    [pOption2View autoPinEdgeToSuperviewEdge:ALEdgeTop withInset:50];
+    
 }
 
 
